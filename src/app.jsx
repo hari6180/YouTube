80435ed7942b3f919316cccb77ad3609f4d76427
hari <ori080525@gmail.com>
2021-07-03 20:46:37 +0900

    show video list

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,31 @@
 import "./app.css";
-import List from "./components/list";
 import Navbar from "./components/navbar";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import VideoList from "./components/video_list";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyArLCzfD95qK4vKvLn-y9Vv1qlE91wa9y0&part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyArLCzfD95qK4vKvLn-y9Vv1qlE91wa9y0",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+
   return (
     <>
       <Navbar />
-      <List />
+      <VideoList videos={videos} />
     </>
   );
 }
---
+++ src/components/video_list.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./list.module.css";
+import VideoItem from "./video_item";
+
+const VideoList = (props) => {
+  return (
+    <ul>
+      {props.videos.map((video) => (
+        <VideoItem key={video.id} video={video} />
+      ))}
+    </ul>
+  );
+};
+
+export default VideoList;
